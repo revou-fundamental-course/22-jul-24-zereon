fe8cf1dc0e5118b7138d69dc34ec2fd0f3c574ef
zereon <ignasius.eric@gmail.com>
2024-08-01 00:00:03 +0700

    Responsive-Final

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ function formValidation() {
     }
 }
 
+//Call us button yang berisi prompt untuk input kontak
 function callAction(){
     let call = prompt("Masukkan email Anda", "");
     console.log(call);
@@ -29,3 +30,27 @@ function callAction(){
     document.getElementById("btn-call").style.display = "none"
 
 }
+//Auto Slide Banner
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n){
+var i;
+var imgList = document.getElementsByClassName("img-slideshow");
+if(n > imgList.length) slideIndex = 1;
+else if (n < 1) slideIndex = imgList.length;
+
+for(i = 0; i < imgList.length; i++) {
+    imgList[i].style.display = "none";
+}
+imgList[slideIndex - 1].style.display = 'block';
+}
+
+setInterval(() => {
+plusDivs(1);
+}, 1000);
